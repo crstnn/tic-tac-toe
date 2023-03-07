ba85ead9fc1e6c80f8fed10e850760763b1cacdc
crstnn <54381541+crstnn@users.noreply.github.com>
2023-03-07 23:12:17 +1100

    feat: parameterise tests to allow for different board sizes

--- test/board_test.py
+++ test/board_test.py
@@ -1,11 +1,12 @@
+import pytest
 from src.tic_tac_toe import TicTacToe
 from src.state import State
 from src.token import Token
 from collections import namedtuple
 
-import pytest
-
 Position = namedtuple("Position", ["row", "column"])
+
+BOARD_SIZES = [3, 4, 5, 20, 100]
 
 
 def create_diagonal(t: TicTacToe, char: Token, square: Position, direction: Position):
@@ -14,33 +15,38 @@ def create_diagonal(t: TicTacToe, char: Token, square: Position, direction: Posi
         square = Position(square.row + direction.row, square.column + direction.column)
 
 
-def test_winner_diagonal1():
-    t = TicTacToe()
+@pytest.mark.parametrize("board_size", BOARD_SIZES)
+def test_winner_diagonal1(board_size):
+    t = TicTacToe(board_size=board_size)
     create_diagonal(t, Token.NAUGHT, Position(0, 0), Position(1, 1))
     assert t.check_state() == State.NAUGHT_WON
 
 
-def test_winner_diagonal2():
-    t = TicTacToe()
+@pytest.mark.parametrize("board_size", BOARD_SIZES)
+def test_winner_diagonal2(board_size):
+    t = TicTacToe(board_size=board_size)
     create_diagonal(t, Token.CROSS, Position(t._BOARD_SIZE - 1, t._BOARD_SIZE - 1), Position(-1, -1))
     assert t.check_state() == State.CROSS_WON
 
 
-def test_winner_horizontal():
-    t = TicTacToe()
+@pytest.mark.parametrize("board_size", BOARD_SIZES)
+def test_winner_horizontal(board_size):
+    t = TicTacToe(board_size=board_size)
     t._board[0] = [Token.CROSS for _ in range(t._BOARD_SIZE)]
     assert t.check_state() == State.CROSS_WON
 
 
-def test_winner_vertical():
-    t = TicTacToe()
+@pytest.mark.parametrize("board_size", BOARD_SIZES)
+def test_winner_vertical(board_size):
+    t = TicTacToe(board_size=board_size)
     for idx in range(t._BOARD_SIZE):
         t._board[idx][0] = Token.NAUGHT
     assert t.check_state() == State.NAUGHT_WON
 
 
-def test_change_of_turn():
-    t = TicTacToe()
+@pytest.mark.parametrize("board_size", BOARD_SIZES)
+def test_change_of_turn(board_size):
+    t = TicTacToe(board_size=board_size)
     t.place_marker(Token.CROSS, 0, 0)
     t.place_marker(Token.NAUGHT, 2, 1)
     t.place_marker(Token.CROSS, 1, 1)
